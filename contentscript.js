4a0108a0335627edb36e773093331be4630d45b6
Pham Duy Dong <dongpd@exoplatform.com>
2014-03-06 16:54:00 +0700

    Only calling eXo ret service if mouse over 2 seconds to avoid crazy mouse moving

--- contentscript.js
+++ contentscript.js
@@ -40,32 +40,41 @@ function bindEventToGmail() {
 Bind mouse hover event to email label
 */
 function bindEmailMouseOverEvent() {
-  $("span[email]").bind("mouseenter", function (event) {
-    event.preventDefault();
-    $('#rightPanel').text("");
+  $("span[email]").hover(function () {
+      var obj = $(this);
+      $('#rightPanel').text("Loading eXo User information...");
 
-    // Get eXo userId
-    var email = $(this).attr("email");
-    var userid = email.substring(0, email.indexOf('@'));
+      // Get eXo userId
+      var email = $(this).attr("email");
+      var userid = email.substring(0, email.indexOf('@'));
 
-    // Get eXo user information by RESTful Service and show on customized right panel.
-    var url = eXoHostName + "/rest/social/people/getPeopleInfo/" + userid + ".json";
-    $.ajax({
-      url: url,
-      dataType: "text"
-    }).done(function (data) {
-      var member = $.parseJSON(data);
-        $('#rightPanel').html("<span>eXo user information</span>" + "</br></br>"
-                          + "Full Name:" + checkNullInfo(member.fullName) + "</br>"
-                          + "Position:" + checkNullInfo(member.position) + "</br>"
-                          + "Last Activity: " + checkNullInfo(member.activityTitle) + "</br>"
-                          + "<img src='" + eXoHostName + member.avatarURL + "'>");
-    }).fail(function () {
-      $('#rightPanel').text("Can not get information");
+      // Get eXo user information by RESTful Service and show on customized right panel.
+      var url = eXoHostName + "/rest/social/people/getPeopleInfo/" + userid + ".json";
+      
+      obj.addClass('active');
+      setTimeout(function () { // Only process hover handler if mouse over email 2 seconds. This avoid crazy mouse moving
+        if (obj.hasClass('active')) {
+          $.ajax({
+            url: url,
+            dataType: "text"
+          }).done(function (data) {
+            var member = $.parseJSON(data);
+            $('#rightPanel').html("<table><tr><td align='left'><span>eXo user information</span>" + "</br></br></td></tr>" 
+                                       + "<tr><td>Email:" + email + "</br></td></tr>" 
+                                       + "<tr><td>Full Name:" + checkNullInfo(member.fullName) + "</br></td></tr>"
+                                       + "<tr><td>Position:" + checkNullInfo(member.position) + "</br></td></tr>"
+                                       + "<tr><td>Activity Title: " + checkNullInfo(member.activityTitle) + "</br></td></tr>" 
+                                       + "<tr><td><img src='" + eXoHostName + member.avatarURL + "'></td></tr>");
+          }).fail(function () {
+            $('#rightPanel').text("Can not get information");
+          });
+        }
+      }, 2000);
+    },
+    function () {
+      $(this).removeClass('active');
+      $('#rightPanel').text("");
     });
-
-    event.preventDefault();
-  });
 }
 
 function checkNullInfo(value) {
